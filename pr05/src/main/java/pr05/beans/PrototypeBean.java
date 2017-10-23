bea1178f0f3ca49f235aece85234b3065ba08f71
Branko Milosavljevic <mbranko@uns.ac.rs>
2017-10-24 00:25:40 +0200

    Dodat primer 05 - bean scope u Spring aplikacijama

---
+++ pr05/src/main/java/pr05/beans/PrototypeBean.java
@@ -0,0 +1,15 @@
+package pr05.beans;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class PrototypeBean {
+
+  private int count = 0;
+
+  public int getCount() {
+    return ++count;
+  }
+}
